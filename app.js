c559b87c7085f9962cc5b857355eef98a6232221
PhillipFeldman <pfeldman@berkeley.edu>
2023-01-13 21:19:54 -0800

    stoppedAtPartials

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ app.set('views','views');
 app.listen(3000);
 
 app.get('/',(req,res)=>{//url here
-res.render('index')})//filename here
+res.render('index', {loggedIn:false})})//filename here,
 
 app.get('/index',(req,res)=>{
 res.redirect('/')})
 
 app.get('/about',(req,res)=>{
-    res.render('about')})
+    res.render('about', {loggedIn:false})})
 
 app.get('/signup',(req,res)=>{
-        res.render('signup')})
+        res.render('signup', {loggedIn:false})})
 
 app.get('/login',(req,res)=>{
-    res.render('login')})
+    res.render('login', {loggedIn:false})})
 
 
 
