0f1c9cb8252a74a564e21363b9bca477bef4230d
PhillipFeldman <pfeldman@berkeley.edu>
2023-01-16 16:25:45 -0800

    Having trouble with post request

--- app.js
+++ app.js
@@ -19,11 +19,14 @@ app.use(express.static('public'));
 //put folder name in parens
 
 
+app.use(express.urlencoded({extended:true}));//to handle post requests of form data
+
+
 
 
 app.get('/',(req,res)=>{//url here
     Account.find().then((result) =>{
-        res.render('index', {accounts:result})
+        res.render('index', {firstp:"Welcome to the",accounts:result})
 
     }).catch((err) =>{console.log(err)})
 
@@ -36,7 +39,7 @@ res.redirect('/')})
 
 app.get('/about',(req,res)=>{
     Account.find().then((result) =>{
-        res.render('about', {accounts:result})
+        res.render('about', {firstp:"About the",accounts:result})
 
     }).catch((err) =>{console.log(err)})
 
@@ -46,7 +49,7 @@ app.get('/about',(req,res)=>{
 
 app.get('/signup',(req,res)=>{
     Account.find().then((result) =>{
-        res.render('signup', {accounts:result})
+        res.render('signup', {firstp:"Sign up to the",accounts:result})
 
     }).catch((err) =>{console.log(err)})
 
@@ -54,9 +57,15 @@ app.get('/signup',(req,res)=>{
 
     })
 
+app.post('/new_account',((req,res)=>{
+    const acc = new Account(req.body.email,req.body.password,false)//req.body.<acc property>, loggedIn = false
+    acc.save().then((result)=>{res.redirect('/login')}).catch((err)=>{console.log(err)})
+console.log(req.body)
+}))
+
 app.get('/login',(req,res)=>{
     Account.find().then((result) =>{
-        res.render('login', {accounts:result})
+        res.render('login', {firstp:"Login to the",accounts:result})
 
     }).catch((err) =>{console.log(err)})
 
@@ -80,7 +89,7 @@ app.get('/add-account',(req,res)=>{
 //404 at the bottom
 app.use((req,res)=>{
     Account.find().then((result) =>{
-        res.status(404).render('404', {accounts:result})
+        res.status(404).render('404', {firstp:"404 to the",accounts:result})
 
     }).catch((err) =>{console.log(err)})
 
