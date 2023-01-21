c14b444af9e8558b5406cd6a9399ed476706a5ad
PhillipFeldman <pfeldman@berkeley.edu>
2023-01-21 00:39:36 -0800

    Some finishing touches

--- app.js
+++ app.js
@@ -35,15 +35,42 @@ app.get('/',(req,res)=>{//url here
 
     })
 
-    app.get('/create-blog',(req,res)=>{//url here
+    app.get('/create-blog/',(req,res)=>{//url here
+        
         Account.find().then((result) =>{
-            res.render('create-blog', {firstp:"Create a Blog!",accounts:result})
+            var a = true;
+            for(var i =0; i< result.length; i++){
+                if(result[i].loggedIn==true){
+                    console.log(`create-blog/${result[i].id}`)
+                    const id = result[i].id;
+            console.log(id)
+            Account.find().then((result1) =>{
+                Account.findById(id).then(result2 => {
+                    res.render('create-blog', {firstp:"Create a Blog!",accounts:result1,currAcc:result2.email})
+                })
+                
+        
+            }).catch((err) =>{console.log(err)})   
+                    a = false
+                }
+            }
+            if(a){
+            res.render('create-blog', {firstp:"Create a Blog!",accounts:result})}
     
         }).catch((err) =>{console.log(err)})
-    
-    
-    
         })
+
+        app.get('/create-blog/:id',(req,res)=>{//url here
+            const id = req.params.id;
+            console.log(id)
+            Account.find().then((result) =>{
+                Account.findById(id).then(result1 => {
+                    res.render('create-blog', {firstp:"Create a Blog!",accounts:result,currAcc:result1})
+                })
+                
+        
+            }).catch((err) =>{console.log(err)})   
+            })
 
 app.get('/index',(req,res)=>{
 res.redirect('/')})
